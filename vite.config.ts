ae469b61decfaecfd90c769ca343f074802b9c5b
Lukas <lukasneuschl@gmail.com>
2026-03-25 21:02:04 +0100

    w

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import instruckt from 'instruckt/vite'
 
 export default defineConfig({
     plugins: [
+        instruckt({ server: false, endpoint: '/instruckt', adapters: ['vue', 'blade'], mcp: true }),
         laravel({
             input: ['resources/js/app.ts'],
             ssr: 'resources/js/ssr.ts',
